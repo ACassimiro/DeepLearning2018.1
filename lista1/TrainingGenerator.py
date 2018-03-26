c68e92850b8d2829364425be0bfdb86825f11772
Igor Malheiros <igormalheiros@MacBook-Pro-de-Igor.local>
2018-03-26 11:54:38 -0300

    training files done - exercise 1

---
+++ lista1/TrainingGenerator.py
@@ -0,0 +1,26 @@
+import random 
+
+f1=open("cubeSample1.txt", "r")
+f = f1.readlines()
+i = 0
+for line in f:
+	x = line.split()
+
+	f2 = open("Noise " + str(i+1)+".txt", "w")
+
+	for j in range(15):
+		(a,b,c) = (float(x[0]), float(x[1]), float(x[2]))
+		a += random.uniform(-0.1, 0.1)
+		b += random.uniform(-0.1, 0.1)
+		c += random.uniform(-0.1, 0.1)
+		l = [str(a), str(b), str(c)] + x[3:]
+		s = ' '.join(l)
+		s += '\n'
+
+		f2.write(s)
+
+	f2.close()
+	i+=1
+
+f1.close()
+
