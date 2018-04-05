72f2e6f2dac98514b62f1d034aad65962ca39510
acassimi <cassimiroaellison@gmail.com>
2018-04-05 19:59:21 -0300

    stoc_training concluída

--- lista1/neuralnet.py
+++ lista1/neuralnet.py
@@ -69,12 +69,49 @@ class NeuralNet():
 
         return outputLayers
 
-    def training(self, input_data, output_data, iterations, learning_rate):
-        for it in range(iterations):
 
+    def stoc_training(self, input_data, output_data, iterations, learning_rate):
+        
+        for it in range(iterations):
+            for k in (range(len(input_data))):
+                
+                outs = self.forward([input_data[k]])
+                outs[0] = outs[0]
+                erro = [None] * len(outs)
+                delta = [None] * len(outs)
+
+                print()
+                print(outs)
+                print()
+
+                #Propaga o erro pra trás
+                for i in range((len(outs)-1),-1,-1):
+                    #Caso especial pra a saída da rede
+                    if i == (len(outs)-1):
+                        erro[i] = output_data[k] - outs[i]
+                    else:
+                        erro[i] = np.dot(delta[i+1], self.layer[i+1].layer_weights.T)
+
+                    delta[i] = erro[i] * self.activation_func(self.func_type_, outs[i])
+
+
+                #Atualiza os pesos
+                for i in range(len(outs)):
+                    #Caso especial pra entrada da rede
+                    if i == 0:
+                        self.layer[i].layer_weights += learning_rate * np.dot(np.array([input_data[k]]).T, delta[i])
+                    else:
+                        self.layer[i].layer_weights += learning_rate * np.dot(outs[i-1].T, delta[i])
+
+
+
+    def batch_training(self, input_data, output_data, iterations, learning_rate):
+        for it in range(iterations):
             outs = self.forward(input_data)
             erro = [None] * len(outs)
             delta = [None] * len(outs)
+
+            print(outs)
 
             #Propaga o erro pra trás
             for i in range((len(outs)-1),-1,-1):
@@ -86,6 +123,7 @@ class NeuralNet():
 
                 delta[i] = erro[i] * self.activation_func(self.func_type_, outs[i])
 
+
             #Atualiza os pesos
             for i in range(len(outs)):
                 #Caso especial pra entrada da rede
@@ -93,6 +131,7 @@ class NeuralNet():
                     self.layer[i].layer_weights += learning_rate * np.dot(input_data.T, delta[i])
                 else:
                     self.layer[i].layer_weights += learning_rate * np.dot(outs[i-1].T, delta[i])
+
 
     def print_weights(self):
         for i in range(self.n_layer):
@@ -106,16 +145,17 @@ if __name__ == "__main__":
 
     nn = NeuralNet([layer1, layer2], 2, "sigmoid")
 
-    print("1) Random weighs")
-    nn.print_weights()
+    #print("1) Random weighs")
+    #nn.print_weights()
 
     X_training,Y_training = parseXORInput("NoiseXOR.txt")
 
     input_data = np.array(X_training)
     output_data = np.array(Y_training).T
+    #print(output_data)
 
-    nn.training(input_data, output_data, 60000, 0.2)
-
+    nn.stoc_training(input_data, output_data, 1000, 0.2)
+    
     print("2) Weighs after training")
     nn.print_weights()
 
@@ -130,3 +170,4 @@ if __name__ == "__main__":
 
         print("Prediceted : " + str(out[len(out)-1]))
         print("Expected : " + str(Y_test[0][x]))
+    
